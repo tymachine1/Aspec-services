bd0b692ae6dc7f907c42e2e468896ca9e4d324da
tymachine1 <73142902+tymachine1@users.noreply.github.com>
2023-02-23 20:30:02 -0500

    Update updates.js

--- Site/updates.js
+++ Site/updates.js
@@ -1,5 +1,5 @@
 var updates = [
-    {message: "Welcome to the 3rd official release of Andyum!"}
+    {message: "© Copyright Aspec Foundation all rights reserved."}
 ];
 var updatespage = document.getElementById("updatespage");
 for(let item of updates) {
